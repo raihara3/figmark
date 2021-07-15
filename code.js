5445ed5bf448071766dacdeb060b7c4c12a03d9d
raihara3 <raihara3@gmail.com>
2021-07-15 23:34:34 +0900

    add clientStorage

--- code.js
+++ code.js
@@ -1,29 +1,29 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-// This shows the HTML page in "ui.html".
+let figmark = [];
 figma.showUI(__html__);
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
 figma.ui.onmessage = msg => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-rectangles') {
-        const nodes = [];
-        for (let i = 0; i < msg.count; i++) {
-            const rect = figma.createRectangle();
-            rect.x = i * 150;
-            rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-            figma.currentPage.appendChild(rect);
-            nodes.push(rect);
+    if (msg.type === "add-bookmark") {
+        // TODO: 複数選択してたら全部追加できるようにする
+        const select = figma.currentPage.selection;
+        const isSaved = figmark.map(v => v.id).filter(id => id === select[0].id);
+        if (isSaved.length > 0) {
+            console.log("既に保存済み");
+            return;
         }
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
+        figmark.push({
+            id: select[0].id,
+            name: select[0].name
+        });
+        figma.clientStorage.setAsync("figmark", figmark);
+        figma.ui.postMessage({ type: "update-figmark", value: figmark });
     }
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
+    else if (msg.type === "select-node") {
+        const node = figma.getNodeById(msg.id);
+        figma.currentPage.selection = new Array().concat(node);
+    }
 };
+figma.clientStorage.getAsync("figmark").then(value => {
+    if (value !== undefined) {
+        figmark = value;
+    }
+    figma.ui.postMessage({ type: "update-figmark", value: figmark });
+});
